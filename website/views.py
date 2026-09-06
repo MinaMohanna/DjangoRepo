f8767a9a08dc3b27925019881f06f8b9af335628
MinaMohanna <minajoya68@gmail.com>
2026-09-06 17:19:01 +0330

    fixed an template does not exist error at file

--- website/views.py
+++ website/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
 def index_view(request):
-    return HttpResponse (" <h1> This is my HOME </h1>")
+    return render(request, "website/home.html")
 
 
 def contact_view(request):
-    return HttpResponse (" <h1> This is my Contact </h1>")
+    return render (request, "website/contact.html")
 
 def about_view(request):
-    return HttpResponse (" <h1> This is my About </h1>")
+    return render (request, "website/about.html")
     

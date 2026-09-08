5e719a63a58aa3ad4caaa1108dbf0eedc81b5b6d
MinaMohanna <minajoya68@gmail.com>
2026-09-09 02:20:45 +0330

    I have made my dirctories and my template files

--- website/views.py
+++ website/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
 def index_view(request):
-    return render(request, "website/home.html")
+    return render(request, "website/index.html")
 
 
 def contact_view(request):

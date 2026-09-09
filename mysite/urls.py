28fb7ba00ced58badde18f14e2de6a13d8254259
MinaMohanna <minajoya68@gmail.com>
2026-09-09 21:52:15 +0330

    I made my blog and single blog urls and views

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,4 +1,5 @@
 """
+
 URL configuration for mysite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,7 +14,10 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    
+
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -22,6 +26,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("website.urls")),
+    path('blog/', include("blog.urls")),
 ]
 
 
